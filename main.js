81d3906c42e22bea156f2b719754be88c9e92b87
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-05-07 08:18:34 -0500

    feat: :sparkles: Listado de ferias funcionando con su LocalStorage

--- main.js
+++ main.js
@@ -23,3 +23,40 @@ let newItem =
     localStorage.setItem("guardado", JSON.stringify(data))
 })
 console.log(data)
+
+class listaFerias extends HTMLElement{
+    constructor(){
+        super()
+        const shadow = this.attachShadow({mode :"open"})
+    
+
+const style = document.createElement("style")
+style.textContent =`
+.card{
+    margin:20px 0 0 20px ;
+    border: 2px solid blue;
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+    width: 700px;
+    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+`
+
+data.forEach(element => {
+    const card = document.createElement("div")
+    card.classList.add("card")
+    card.innerHTML = `
+            <p><b>Nombre del Evento: </b>${element.nombre} </p>
+            <p><b>Lugar: </b>${element.lugar}  </p>
+            <p><b>Fecha de inicio:</b> ${element.inicio} </p>
+            <p><b>Fecha de finalización:</b> ${element.final} </p>
+            <p><b>Horarios:</b> ${element.horario} </p>
+    `
+    shadow.appendChild(card)
+});
+shadow.appendChild(style)
+}
+}
+
+customElements.define("lista-ferias", listaFerias)
